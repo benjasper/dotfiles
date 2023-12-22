45fe53845a9604b2a67302c5076ce888cafd89cf
Benjamin Jasper <benjamin.jasper@itx.de>
2023-12-22 13:09:42 +0100

    feat: added search without gitignore

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,12 +1,15 @@
 require('telescope').setup{}
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', function () builtin.find_files({no_ignore=true}) end, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 vim.keymap.set('n', '<leader>pc', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>cc', builtin.registers, {})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>ds', builtin.diagnostics, { buffer = 0 })
+vim.keymap.set('n', '<leader>ds', function ()
+	return builtin.diagnostics({bufnr = 0})
+end
+, {})
 
 require('telescope').load_extension('fzf')
