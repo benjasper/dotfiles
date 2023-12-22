6e0ed9299a4a7e38771613aa6ee7555b6f3cc16e
Benjamin Jasper <benjamin.jasper@itx.de>
2023-12-22 08:54:12 +0100

    feat: added telescope diagnostics and fzf

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,5 @@
+require('telescope').setup{}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
@@ -5,3 +7,6 @@ vim.keymap.set('n', '<leader>pc', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>cc', builtin.registers, {})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>ds', builtin.diagnostics, { buffer = 0 })
+
+require('telescope').load_extension('fzf')
