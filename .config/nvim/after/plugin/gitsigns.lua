6c9ad25db49d1e24a5781ccf98d99b821d2f787a
Benjamin Jasper <jasper.benni@gmail.com>
2023-12-27 08:32:14 +0100

    feat: added gitsign bindings and small other fixes

--- .config/nvim/after/plugin/gitsigns.lua
+++ .config/nvim/after/plugin/gitsigns.lua
@@ -1,2 +1,14 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+	on_attach = function (bufnr)
+		local gs = package.loaded.gitsigns
+
+		vim.keymap.set('n', '<leader>hs', gs.stage_hunk)
+		vim.keymap.set('n', '<leader>hr', gs.reset_hunk)
+		vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame)
+		vim.keymap.set('n', '<leader>hd', gs.diffthis)
+		vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end)
+		vim.keymap.set('n', '<leader>td', gs.toggle_deleted)
+	end
+})
+
 
