2fa32e3c6af3bc82148907de410d7e9c7c79e54f
Benjamin Jasper <jasper.benni@gmail.com>
2024-01-04 21:16:52 +0100

    feat(nvim): added better paste and cut handling

--- .config/nvim/lua/benjasper/remap.lua
+++ .config/nvim/lua/benjasper/remap.lua
@@ -2,3 +2,7 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-H>", "20zh")
 vim.keymap.set("n", "<C-L>", "20zl")
+
+vim.keymap.set("n", "<C-P>", "\"0p")
+vim.keymap.set("v", "<C-c>", "\"+y")
+
