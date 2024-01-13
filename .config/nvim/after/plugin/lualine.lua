5d4f1de15d757162ccfde5c0e7613a4b5a1a304c
Benjamin Jasper <jasper.benni@gmail.com>
2024-01-13 18:14:26 +0100

    fix: fixed lualine being open inside nvim tree

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -4,10 +4,7 @@ require('lualine').setup {
 		theme = 'catppuccin',
 		component_separators = { left = '', right = '' },
 		section_separators = { left = '', right = '' },
-		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
-		},
+		disabled_filetypes = { 'packer', 'NvimTree' };
 		ignore_focus = {},
 		always_divide_middle = true,
 		globalstatus = false,
