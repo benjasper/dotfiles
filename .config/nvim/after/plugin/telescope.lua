696562e58aeb74e9f7c33edd2b5035e63d79d2e0
Benjamin Jasper <benjamin.jasper@itx.de>
2024-01-26 09:28:19 +0100

    feat: added telescope previous seach

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -32,6 +32,8 @@ require('telescope').setup {
 
 vim.keymap.set('n', '<leader>pc', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pac', function() builtin.live_grep({ additional_args = { "-u" } }) end, {})
+vim.keymap.set('n', '<leader>pl', builtin.resume, {})
+
 vim.keymap.set('n', '<leader>po', builtin.oldfiles, {})
 vim.keymap.set('n', '<leader>cc', builtin.registers, {})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
