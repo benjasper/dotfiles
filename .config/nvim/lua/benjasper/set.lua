6a3aab65a75788781c147374d6254e83d467e682
Benjamin Jasper <jasper.benni@gmail.com>
2024-02-01 21:18:44 +0100

    feat: added hightlight on yank

--- .config/nvim/lua/benjasper/set.lua
+++ .config/nvim/lua/benjasper/set.lua
@@ -29,3 +29,14 @@ vim.g.mapleader = " "
 vim.opt.title = true
 vim.opt.titlelen = 0 -- do not shorten title
 vim.opt.titlestring = 'nvim %{expand("%:f")}'
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
