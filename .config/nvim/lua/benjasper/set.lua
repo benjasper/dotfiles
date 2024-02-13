488b613bebc810a52e88103dfdb4b6884deefa1c
Benjamin Jasper <jasper.benni@gmail.com>
2024-02-13 21:39:55 +0100

    feat: added a few big improvements to neovim config

--- .config/nvim/lua/benjasper/set.lua
+++ .config/nvim/lua/benjasper/set.lua
@@ -30,13 +30,16 @@ vim.opt.title = true
 vim.opt.titlelen = 0 -- do not shorten title
 vim.opt.titlestring = 'nvim %{expand("%:f")}'
 
+vim.o.foldcolumn = '1'
+vim.o.splitright = true
+
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-  group = highlight_group,
-  pattern = '*',
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = '*',
 })
