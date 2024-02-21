0d00eeeb0f885a20ca7d1ff32784f67603be24c9
Benjamin Jasper <jasper.benni@gmail.com>
2024-02-21 18:32:53 +0100

    feat: made a few improvements to lualine config and better keymappings for harpoon

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -4,7 +4,7 @@ require('lualine').setup {
 		theme = 'catppuccin',
 		component_separators = { left = '', right = '' },
 		section_separators = { left = '', right = '' },
-		disabled_filetypes = { 'packer', 'neo-tree' };
+		disabled_filetypes = { 'packer', 'neo-tree' },
 		ignore_focus = {},
 		always_divide_middle = true,
 		globalstatus = false,
@@ -16,16 +16,16 @@ require('lualine').setup {
 	},
 	sections = {
 		lualine_a = { '[[]]', { 'mode', padding = { left = 0, right = 1 } } },
-		lualine_b = { 'location', 'diagnostics' },
-		lualine_c = { { 'branch', padding = { left = 2, right = 1 } }, '%=' },
+		lualine_b = { 'location', 'progress' },
+		lualine_c = { { 'branch', padding = { left = 2, right = 1 } }, 'diagnostics', '%=' },
 		lualine_x = { require('lsp-progress').progress },
-		lualine_y = { { 'filetype', icon_only = true, padding = { left = 1, right = 0 } }, { 'filename', file_status = true } },
+		lualine_y = { { 'filetype', icon_only = true, padding = { left = 1, right = 0 } }, { 'filename', path = 1, file_status = true } },
 		lualine_z = { { '[[]]' }, { 'vim.fn.fnamemodify(vim.fn.getcwd(), ":t")', path = 4, padding = { left = 0, right = 1 } } }
 	},
 	inactive_sections = {
 		lualine_a = {},
 		lualine_b = {},
-		lualine_c = { 'filename' },
+		lualine_c = { { 'filename', path = 1 } },
 		lualine_x = { 'location' },
 		lualine_y = {},
 		lualine_z = {}
