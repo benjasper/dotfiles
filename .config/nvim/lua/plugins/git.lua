de87e05b0d7043652b530dee23959321d75366c4
Benjamin Jasper <jasper.benni@gmail.com>
2024-03-17 18:09:52 +0100

    feat: added more improvements to nvim config

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -9,10 +9,15 @@ return {
 					vim.keymap.set('n', '<leader>hs', gs.stage_hunk)
 					vim.keymap.set('n', '<leader>hr', gs.reset_hunk)
 					vim.keymap.set('n', '<leader>tb', gs.toggle_current_line_blame)
-					vim.keymap.set('n', '<leader>hd', gs.diffthis)
-					vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end)
 					vim.keymap.set('n', '<leader>td', gs.toggle_deleted)
-				end
+					vim.keymap.set('n', '<leader>hd', gs.diffthis)
+					vim.keymap.set('n', '<leader>hp', gs.preview_hunk)
+					vim.keymap.set('n', '<leader>hD', function() gs.diffthis('~') end)
+					vim.keymap.set('n', '<leader>hb', function() gs.blame_line{full=true} end)
+				end,
+				current_line_blame_opts = {
+					delay = 0
+				}
 			})
 		end
 	},
