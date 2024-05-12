07b096d7d20229dfad7189cca13dc6cb00120db4
Benjamin Jasper <jasper.benni@gmail.com>
2024-05-12 18:08:22 +0200

    feat: added supermaven ai

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,31 +2,6 @@
 return {
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
-	{
-		'Exafunction/codeium.vim',
-		keys = { {
-			"<leader>ce",
-			"<cmd>:CodeiumEnable<CR>",
-			mode = { "n", "x" }
-		}, {
-			"<leader>cd",
-			"<cmd>:CodeiumDisable<CR>"
-		} },
-		event = 'BufEnter',
-		config = function()
-			vim.g.codeium_disable_bindings = 1
-
-			vim.g.codeium_enabled = false
-
-			vim.keymap.set('i', '<C-y>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-			vim.keymap.set('i', '<c-.>', function() return vim.fn['codeium#CycleCompletions'](1) end,
-				{ expr = true, silent = true })
-			vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
-				{ expr = true, silent = true })
-			vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-		end
-	},
-
 	-- "gc" to comment visual regions/lines
 	{
 		"numToStr/Comment.nvim",
