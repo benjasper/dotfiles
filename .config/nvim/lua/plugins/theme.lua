6d3b45ae5d19887351734a64103df5c4c1afa3d0
Benjamin Jasper <jasper.benni@gmail.com>
2024-08-10 12:53:27 +0200

    feat: updated dependencies

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -6,6 +6,7 @@ return {
 		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
 		"catppuccin/nvim",
 		name = "catppuccin",
+		version = "*",
 		lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
