6843f5075a4f6d6ebcbd7c461c7f1b5a57cd01f0
Benjamin Jasper <jasper.benni@gmail.com>
2024-09-25 17:53:45 +0200

    feat: upgraded deps including fix for telescope additional args

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -22,9 +22,6 @@ return {
 			{ "nvim-tree/nvim-web-devicons" },
 			{
 				"nvim-telescope/telescope-live-grep-args.nvim",
-				-- This will not install any breaking changes.
-				-- For major updates, this must be adjusted manually.
-				version = "^1.0.0",
 			},
 		},
 		config = function()
