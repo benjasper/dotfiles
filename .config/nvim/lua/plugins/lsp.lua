6bc4d65227bba8e9585737775177c74f76b3e0f6
Benjamin Jasper <jasper.benni@gmail.com>
2025-01-04 11:14:08 +0100

    fix(nvim): fixed weird snippet highlighting issue by (temporarily) switching from vim.snippet to luasnip

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -183,13 +183,20 @@ return {
 		},
 	},
 	{
+		'L3MON4D3/LuaSnip',
+		version = "v2.*",
+		dependencies = { "rafamadriz/friendly-snippets" },
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{
 		'saghen/blink.cmp',
 		lazy = false, -- lazy loading handled internally
-		-- optional: provides snippets for the snippet source
-		dependencies = 'rafamadriz/friendly-snippets',
+		dependencies = {{ 'L3MON4D3/LuaSnip', version = 'v2.*' }},
 
 		-- use a release tag to download pre-built binaries
-		version = 'v0.*',
+		version = '*',
 		-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 		-- build = 'cargo build --release',
 		-- If you use nix, you can build from source using latest nightly rust with:
@@ -215,10 +222,19 @@ return {
 				nerd_font_variant = 'mono',
 			},
 
-			-- default list of enabled providers defined so that you can extend it
-			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			-- Use luasnip as a snippet engine for now, because of weird highlighting issues with vim.snippet https://www.reddit.com/r/neovim/comments/1fj1gbl/highlighted_text_when_expanding_snippets_using/
+			snippets = {
+				expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
+				active = function(filter)
+					if filter and filter.direction then
+						return require('luasnip').jumpable(filter.direction)
+					end
+					return require('luasnip').in_snippet()
+				end,
+				jump = function(direction) require('luasnip').jump(direction) end,
+			},
 			sources = {
-				default = { 'lsp', 'path', 'snippets', 'buffer' },
+				default = { 'lsp', 'path', 'luasnip', 'buffer' },
 				-- disable cmdline completions
 				cmdline = {},
 			},
