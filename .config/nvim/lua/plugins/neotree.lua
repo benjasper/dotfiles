f2a3ac0a784527fee3a8887a2f3abe933576ea21
Benjamin Jasper <benjamin.jasper@itx.de>
2025-01-31 11:17:47 +0100

    fix(nvim): fixed neotree event error when opening directory

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -83,7 +83,10 @@ return {
 				{
 					event = events.FILE_OPENED,
 					handler = function()
-						vim.fn.setreg("#", vim.w.neo_tree_before_open_visible_buffer)
+						local buf = vim.w.neo_tree_before_open_visible_buffer
+						if buf and vim.api.nvim_buf_is_valid(buf) then
+							vim.api.nvim_set_current_buf(buf)
+						end
 					end,
 				},
 			}
