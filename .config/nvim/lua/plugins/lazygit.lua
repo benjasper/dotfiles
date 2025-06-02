ad615e9964fb6154d0982e6346f34c56a348eddb
Benjamin Jasper <jasper.benjamin@icloud.com>
2025-06-02 21:10:14 +0200

    fix(nvim): disable flatten dep because of issue with neotest

--- .config/nvim/lua/plugins/lazygit.lua
+++ .config/nvim/lua/plugins/lazygit.lua
@@ -2,6 +2,19 @@
 ---@type LazySpec
 return {
 	"mikavilpas/tsugit.nvim",
+	dependencies = {
+		{
+			-- Open files and command output from wezterm, kitty, and neovim terminals in
+			-- your current neovim instance
+			-- https://github.com/willothy/flatten.nvim
+			"willothy/flatten.nvim",
+			-- Ensure that it runs first to minimize delay when opening file from terminal
+			lazy = false,
+			priority = 1001,
+			enabled = false, -- https://github.com/willothy/flatten.nvim/issues/106
+			config = true,
+		},
+	},
 	keys = {
 		{
 			"<leader>gl",
