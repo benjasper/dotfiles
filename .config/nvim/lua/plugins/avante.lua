b344fd664152d46f128d45cb93c97c2df45a5dc6
Benjamin Jasper <jasper.benjamin@icloud.com>
2025-06-16 22:23:07 +0200

    feat(nvim): updated dependencies

---
+++ .config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,27 @@
+return {
+	"yetone/avante.nvim",
+	event = "VeryLazy",
+	version = false, -- Never set this value to "*"! Never!
+	opts = {
+		-- add any opts here
+		-- for example
+		provider = "gemini",
+	},
+	build = "make",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
+		--- The below dependencies are optional,
+		"folke/snacks.nvim",         -- for input provider snacks
+		'echasnovski/mini.icons',
+		{
+			-- Make sure to set this up properly if you have lazy=true
+			'MeanderingProgrammer/render-markdown.nvim',
+			opts = {
+				file_types = { "markdown", "Avante" },
+			},
+			ft = { "markdown", "Avante" },
+		},
+	},
+}
--- .config/nvim/lua/plugins/navstack.lua
+++ .config/nvim/lua/plugins/navstack.lua
@@ -1,16 +1,27 @@
 return {
 	'benjasper/navstack.nvim',
+	-- dir = "~/Workspace/navstack.nvim",
 	dependencies = {
 		{ 'echasnovski/mini.icons', version = '*' }, -- or { 'nvim-tree/nvim-web-devicons', version = '*' }
 	},
 	config = function()
 		local navstack = require("navstack")
+
+		local width = 40
+
 		navstack.setup({
+			win_type = "float",
 			sidebar = {
 				show_current = true,
 				open_on_start = true
 			},
+			direct_jump_as_new_entry = false,
 			quit_when_last_window = true,
+			window_float = {
+				width = width,
+				col = vim.o.columns - width,
+				height = 18,
+			}
 		})
 
 		for i = 1, 9 do
@@ -22,5 +33,6 @@ return {
 		vim.keymap.set("n", "<C-n>", function() navstack.jump_to_next() end, { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>n", function() navstack.toggle_sidebar() end, { noremap = true, silent = true })
 		vim.keymap.set("n", "<leader>cn", function() navstack.clear() end, { noremap = true, silent = true })
+		vim.keymap.set("n", "<leader>pn", function() navstack.toggle_pin() end, { noremap = true, silent = true })
 	end
 }
