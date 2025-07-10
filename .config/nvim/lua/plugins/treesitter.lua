3ffcf6491840622830c2862187c55d7fcaa88535
Benjamin Jasper <benjamin.jasper@check24.de>
2025-07-10 16:26:53 +0200

    feat(nvim): add treesitter folding

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -83,6 +83,10 @@ return {
 				},
 			})
 
+			vim.wo.foldmethod = 'expr'
+			vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+			vim.opt.foldlevel = 99
+
 			-- There are additional nvim-treesitter modules that you can use to interact
 			-- with nvim-treesitter. You should go explore a few and see what interests you:
 			--
