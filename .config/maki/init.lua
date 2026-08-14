5b97c7502ee037638b73035f3a9a7dadd752bc67
Benjamin Jasper <benjamin.jasper@check24.de>
2026-08-14 08:41:06 +0200

    feat(maki): reduce tool output lines to 1

--- .config/maki/init.lua
+++ .config/maki/init.lua
@@ -3,5 +3,16 @@ maki.setup({
 	always_thinking = 'high',
 	ui = {
 		show_thinking = false,
+		tool_output_lines = {
+			bash = 1,
+			code_execution = 1,
+			task = 1,
+			index = 1,
+			grep = 1,
+			read = 1,
+			write = 1,
+			web = 1,
+			other = 1,
+		},
 	}
 })
