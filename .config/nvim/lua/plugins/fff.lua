8be2a6ae634d5fb3db8cbf4081456dad4a2c0f7e
Benjamin Jasper <jasper.benjamin@icloud.com>
2026-09-03 21:48:22 +0200

    feat(nvim): update fff

--- .config/nvim/lua/plugins/fff.lua
+++ .config/nvim/lua/plugins/fff.lua
@@ -1,12 +1,13 @@
 return {
-	'dmtrKovalenko/fff.nvim',
-	version = '*',
+	'dmtrKovalenko/fff',
+	branch = 'main',
 	build = function()
 		-- downloads a prebuilt binary or falls back to cargo build
 		require("fff.download").download_or_build_binary()
 	end,
 	opts = {
 		title = 'Search',
+		lazy_sync = false,
 		prompt = ' ',
 		debug = {
 			enabled = false,
@@ -15,12 +16,16 @@ return {
 		layout = {
 			prompt_position = 'top',
 		},
+		keymaps = {
+			cycle_previous_query = '<C-k>',
+			cycle_forward_query = '<C-j>',
+		}
 	},
 	lazy = false, -- the plugin lazy-initialises itself
 	keys = {
 		{ "<leader>pf",  function() require('fff').find_files() end,                                           desc = 'FFFind files' },
 		{ "<leader>pg",  function() require('fff').find_files() end,                                           desc = 'FFFind files' },
-		{ "<leader>pc",  function() require('fff').live_grep({ grep = { modes = { 'fuzzy', 'plain' } } }) end, desc = 'LiFFFe grep' },
-		{ "<leader>pac", function() require('fff').live_grep({ grep = { modes = { 'fuzzy', 'plain' } } }) end, desc = 'LiFFFe grep' },
+		{ "<leader>pc",  function() require('fff').live_grep({ grep = { modes = { 'plain', 'fuzzy' } } }) end, desc = 'LiFFFe grep' },
+		{ "<leader>pac", function() require('fff').live_grep({ grep = { modes = { 'plain', 'fuzzy' } } }) end, desc = 'LiFFFe grep' },
 	},
 }
